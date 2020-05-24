06f761136312d24e1edbd4a93c680f43326630bd
ematthewrichards <64328257+ematthewrichards@users.noreply.github.com>
2020-05-24 19:21:18 -0400

    Created coffee_machine.py

---
+++ coffee_machine.py
@@ -0,0 +1,15 @@
+coffee_steps = [
+    "Starting to make a coffee",
+    "Grinding coffee beans",
+    "Boiling water",
+    "Mixing boiled water with crushed coffee beans",
+    "Pouring coffee into the cup",
+    "Pouring some milk into the cup",
+    "Coffee is ready!"
+]
+def make_coffee():
+    for steps in coffee_steps:
+        print(steps)
+    exit()
+
+print(make_coffee())
